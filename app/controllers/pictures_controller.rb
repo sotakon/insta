3a9990b3a264f407822056208d9ad68d6952ae15
sotakon <skon1221@gmail.com>
2021-02-13 15:04:24 +0900

    メール機能開発環境のみ適用

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -25,7 +25,7 @@ class PicturesController < ApplicationController
     @picture = current_user.pictures.build(picture_params)
     respond_to do |format|
       if @picture.save
-        # PictureMailer.picture_mail(@picture).deliver
+        PictureMailer.picture_mail(@picture).deliver
         format.html { redirect_to @picture, notice: "Picture was successfully created." }
         format.json { render :show, status: :created, location: @picture }
       else
